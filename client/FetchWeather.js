f84e900c621d17a2e91eb2efa29e19959e491740
Lilly Piri <lilly@lillypiri.com>
2018-01-03 16:45:28 +1000

    weather icons added

--- client/FetchWeather.js
+++ client/FetchWeather.js
@@ -43,6 +43,7 @@ class FetchWeather extends Component {
       .then(weather => {
         this.setState({
           isLoading: false,
+          id: weather.weather[0].id,
           weather: weather.weather[0].description,
           name: weather.name,
           country: weather.sys.country,
@@ -59,6 +60,36 @@ class FetchWeather extends Component {
       <div>
         The current weather in {this.state.name}, {this.state.country} is {this.state.weather},{' '}
         {Math.floor(this.state.temperature - 273.15)}°C and wind speed of {this.state.wind}.
+        {this.state.id >= 200 && this.state.id <= 233 &&
+          <img src="https://openweathermap.org/img/w/11d.png" />
+        } 
+        {this.state.id >= 300 && this.state.id <= 322 && 
+          <img src="https://openweathermap.org/img/w/09d.png" />
+        } 
+        {this.state.id >= 500 && this.state.id <= 510 && 
+          <img src="https://openweathermap.org/img/w/10d.png" />
+        } 
+        {this.state.id >= 511 && this.state.id <= 532 &&
+        <img src="https://openweathermap.org/img/w/09d.png" />
+        }
+        {this.state.id >= 600 && this.state.id <= 623 &&
+        <img src="https://openweathermap.org/img/w/13d.png" />
+        }
+        {this.state.id >= 700 && this.state.id <= 782 &&
+          <img src="https://openweathermap.org/img/w/50d.png" />
+        } 
+        {this.state.id === 800 &&
+          <img src="https://openweathermap.org/img/w/01d.png" />
+        } 
+        {this.state.id === 801 && 
+          <img src="https://openweathermap.org/img/w/02d.png" />
+        } 
+        {this.state.id === 802 && 
+          <img src="https://openweathermap.org/img/w/03d.png" />
+        } 
+        {this.state.id >= 804 && this.state.id <= 805 && 
+          <img src="https://openweathermap.org/img/w/04d.png" />
+        } 
       </div>
     );
   }
