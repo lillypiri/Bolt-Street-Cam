48168f487a48daa9b638fc00d4442124275d2410
Lilly Piri <lilly@lillypiri.com>
2018-01-03 16:07:59 +1000

    missing isomorphic fetch

--- client/main.js
+++ client/main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Container from './Container';
-
+require('es6-promise/auto');
 import './css/custom.css';
 
 ReactDOM.render(<Container />, document.getElementById('root'));
